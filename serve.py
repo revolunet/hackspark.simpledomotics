77bdade17350b36dcb03227255c4b9c995c2ff2f
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2013-10-02 15:16:02 +0000

    initial import

---
+++ serve.py
@@ -0,0 +1,14 @@
+from bottle import route, run, view
+from bottle import static_file
+
+@route('/hello')
+@route('/hello/<name>')
+@view('hello')
+def hello(name='World'):
+    return dict(name=name)
+
+@route('/static/:filename#.*#')
+def send_static(filename):
+    return static_file(filename, root='static/')
+
+run(host='0.0.0.0', port=8080, debug=True)
