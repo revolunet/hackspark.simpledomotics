e7bd1cfa7b2d6bf799cf3a37ff5a05f6e4fd47ca
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2013-10-03 08:04:17 +0000

    namespacing
    --HG--
    rename : hackspark/simpledomotics/__init__.py => src/HackSpark/SimpleDomotics/__init__.py
    rename : hackspark/simpledomotics/app/__init__.py => src/HackSpark/SimpleDomotics/app/__init__.py
    rename : hackspark/simpledomotics/app/auth.py => src/HackSpark/SimpleDomotics/app/auth.py
    rename : hackspark/simpledomotics/app/main.py => src/HackSpark/SimpleDomotics/app/main.py
    rename : hackspark/simpledomotics/app/switches.py => src/HackSpark/SimpleDomotics/app/switches.py
    rename : hackspark/simpledomotics/plugins/__init__.py => src/HackSpark/SimpleDomotics/plugins/__init__.py
    rename : hackspark/simpledomotics/serve.py => src/HackSpark/SimpleDomotics/serve.py
    rename : hackspark/simpledomotics/static/css/bootstrap-theme.css => src/HackSpark/SimpleDomotics/static/css/bootstrap-theme.css
    rename : hackspark/simpledomotics/static/css/bootstrap-theme.min.css => src/HackSpark/SimpleDomotics/static/css/bootstrap-theme.min.css
    rename : hackspark/simpledomotics/static/css/bootstrap.css => src/HackSpark/SimpleDomotics/static/css/bootstrap.css
    rename : hackspark/simpledomotics/static/css/bootstrap.min.css => src/HackSpark/SimpleDomotics/static/css/bootstrap.min.css
    rename : hackspark/simpledomotics/static/fonts/glyphicons-halflings-regular.eot => src/HackSpark/SimpleDomotics/static/fonts/glyphicons-halflings-regular.eot
    rename : hackspark/simpledomotics/static/fonts/glyphicons-halflings-regular.svg => src/HackSpark/SimpleDomotics/static/fonts/glyphicons-halflings-regular.svg
    rename : hackspark/simpledomotics/static/fonts/glyphicons-halflings-regular.ttf => src/HackSpark/SimpleDomotics/static/fonts/glyphicons-halflings-regular.ttf
    rename : hackspark/simpledomotics/static/fonts/glyphicons-halflings-regular.woff => src/HackSpark/SimpleDomotics/static/fonts/glyphicons-halflings-regular.woff
    rename : hackspark/simpledomotics/static/js/bootstrap.js => src/HackSpark/SimpleDomotics/static/js/bootstrap.js
    rename : hackspark/simpledomotics/static/js/bootstrap.min.js => src/HackSpark/SimpleDomotics/static/js/bootstrap.min.js
    rename : hackspark/simpledomotics/views/base.tpl => src/HackSpark/SimpleDomotics/views/base.tpl
    rename : hackspark/simpledomotics/views/hello.tpl => src/HackSpark/SimpleDomotics/views/hello.tpl
    rename : hackspark/simpledomotics/views/main.tpl => src/HackSpark/SimpleDomotics/views/main.tpl
    rename : hackspark/__init__.py => src/HackSpark/__init__.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import os
+
+try:
+    import py2exe
+except ImportError:
+    pass
+
+version = '0.4'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='HackSpark.SimpleDomotics',
+      version=version,
+      description="Simple Domotics from HackSpark",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Jonathan Schemoul',
+      author_email='jonathan.schemoul@gmail.com',
+      url='http://hackspark.fr/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['HackSpark'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """
+      )
